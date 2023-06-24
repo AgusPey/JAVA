259c72e06c17ee9b13c37a8282cc953a947cc571
AgusPey <agustin118@live.com.ar>
2023-06-24 18:39:25 -0300

    Create tarea04.java

---
+++ tarea04.java
@@ -0,0 +1,46 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setEdad(23);
+        persona.setNombre("Agustin");
+        persona.setTelefono("123456789");
+        
+        System.out.println("Edad: " + persona.getEdad());
+        System.out.println("Nombre: " + persona.getNombre());
+        System.out.println("Teléfono: " + persona.getTelefono());
+    }
+}
+
+public class Persona {
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
+
+
+
